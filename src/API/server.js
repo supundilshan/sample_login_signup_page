8c5f8f23d897101b0e030fffb61ab611b909ee1b
supundilshan <supundilshandelpagoda@gmail.com>
2022-08-14 12:48:38 +0530

    login form changed and add variable access token

--- src/API/server.js
+++ src/API/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 app.use(cors());
 
-app.use('/login', (req, res) => {
+app.post('/login', (req, res) => {
+    console.log(req.body.password)
     res.send({
       token: 'test123'
     });
   });
 
-  app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+// app.use('/add').post(function(req,res){
+//     let detail = req.body;
+//     console.log(req.body);
+    // .then(student => {
+    //     res.status(200).json({'student':'student is added'}); 
+    // })
+    // .catch(err =>{
+    //     res.status(400).send("unable to save to DB");
+    // });
+
+// })
+  app.listen(8080, () => console.log('API is running on http://localhost:8080'));
+  
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Navigate} from 'react-router-dom';
+import axios from 'axios';
 import './LoginStyles.css'
 
 class login extends Component {
@@ -8,12 +9,25 @@ class login extends Component {
         this.state = {
             email:'',
             password:'',
-            autheraized:false
+            autheraized:false,
+            accesstoken:'11'
         }
     } 
 
     onSubmithandle = (e) =>{
         e.preventDefault();
+        const detail = {
+            email: this.state.email,
+            password :this.state.password
+        };
+        axios.post('http://localhost:8080/login', detail)
+            .then(res=>{
+                // console.log(res.data.token);
+                this.setState({accesstoken:res.data.token});
+                console.log(this.state.accesstoken);
+            })
+            .catch(err => {console.log(err)})
+
         if(this.state.email == "kasunpk256@gmail.com" && this.state.password == "123"){
             // alert(JSON.stringify(this.state));
             this.setState({goToDashboardPge: true});
